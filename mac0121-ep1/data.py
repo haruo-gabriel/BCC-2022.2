c61a59ad4af3dc1bcb1d0b771dcfe2a1f32011c4
haruo-gabriel <takeuchigabriel@usp.br>
2022-09-10 15:38:36 -0300

    commit

--- mac0121-ep1/data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv(r'passos_frequencia.csv')
-df = pd.DataFrame(data, columns=['Frequencia'])
-print(df)
-print(df.hist())
--- mac0121-ep1/main.c
+++ mac0121-ep1/main.c
@@ -1,56 +1,55 @@
+/* 
+ * Programa principal.
+ * Feito por Gabriel Haruo Hanai Takeuchi 
+ * (NUSP: 13671636) para o EP1 da disciplina
+ * MAC0121. 
+ *
+ * Compilado com: gcc -Wall main.c -o main
+ */ 
+
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAXPASSOS 350
-#define MAXNUM 100000
-
-// Protótipo das funções
-int collatz (int n, int *pot2, int *contPot2);
+// Protótipos
+long int collatz (int n);
 
 // Funções
 int main (){
-	int i, f, x, pot2=2, contPot2=0;
+	long int i, f, num, passos, pot2=2, contPot2=0;
 	
     printf("Digite o primeiro número do intervalo: ");
-    scanf("%d", &i);
+    scanf("%ld", &i);
     printf("Digite o último número do intervalo: ");
-    scanf("%d", &f);
+    scanf("%ld", &f);
 
-    for (x = i; x <= f; x++){
-        collatz(i, &pot2, &contPot2);
+    for (num = i; num <= f; num++){
+		// Verifica se n é potência de 2
+	    if (num == pot2){
+	        pot2 *= 2;
+	        contPot2++;
+	        passos = contPot2;
+	    }
 
-    	// Impressão de dados
-		
+		else
+        	passos = collatz(num);
 
+		printf("Número: %ld - Passos: %ld\n", num, passos);
 	}
-    
-    
 
     return 0;
 }
 
-int collatz (int n, int *pot2, int *contPot2){
+long int collatz (int n){
     int cont = 0;
 
-	// Verifica se n é potência de 2
-    if (n == *pot2){
-        *pot2 *= 2;
-        *contPot2++;
-        return contPot2;
-    }
-
-    // Procedimento padrão da conjectura 
-    else{
-        while (n != 1){
-            if (n % 2 == 0){
-                cont++;
-                n = n/2;    
-            }
-            else{
-                cont++;
-                n = 3*n + 1;
-            }
-        }
-        return cont;
-    }
+	while (n != 1){
+		if (n % 2 == 0){
+			cont++;
+			n = n/2;    
+		}
+		else{
+			cont++;
+			n = 3*n + 1;
+		}
+	}
+	return cont;
 }
