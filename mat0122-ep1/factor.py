75da96ab52a613d01a8885cacae8f11e4d0f73b2
haruo <takeuchigabriel@usp.br>
2022-12-15 19:15:58 -0300

    sync

--- mat0122-ep1/factor.py
+++ mat0122-ep1/factor.py
@@ -1,3 +1,20 @@
+#------------------------------------------------------------------------------
+# # MAT0122 ÁLGEBRA LINEAR I
+# Aluno: <NOME COMPLETO>
+# Numero USP: <NUSP>
+# Tarefa: <Identificação do exercício>
+# Data: <DATA>
+# 
+# Baseado em ... (breve e se aplicável)
+# 
+# DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESTE PROGRAMA.  TODAS AS 
+# PARTES DO PROGRAMA, EXCETO AS QUE SÃO BASEADAS EM MATERIAL FORNECIDO  
+# PELO PROFESSOR OU COPIADAS DO LIVRO OU DO MATERIAL DIDÁTICO DE MAT0122, 
+# FORAM DESENVOLVIDAS POR MIM.  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS 
+# AS CÓPIAS DESTE PROGRAMA E QUE NÃO DISTRIBUÍ NEM FACILITEI A DISTRIBUIÇÃO
+# DE CÓPIAS DESTA PROGRAMA.
+#------------------------------------------------------------------------------
+
 import sys
 import GF2
 from GF2 import one
